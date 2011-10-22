7997390c301ff6dd17cfa5c4d306aa11dcd5cc43
Florian Amstutz <florian@amstutz.nu>
2011-10-22 15:26:16 +0200

    Created solution

---
+++ RunningErrands/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace RunningErrands
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
